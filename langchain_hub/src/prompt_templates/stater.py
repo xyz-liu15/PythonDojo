f245ec96ef80e06ad6412e66c4534c37031dd7e8
xyz-liu15 <xyz.liu15@gmail.com>
2025-05-14 09:57:01 +0800

    feat: 添加stater.py文件，初始化聊天模型并实现基于消息的提示模板

---
+++ langchain_hub/src/prompt_templates/stater.py
@@ -0,0 +1,30 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+llm = ChatOpenAI(model="Pro/deepseek-ai/DeepSeek-V3", base_url="https://www.dmxapi.com/v1", api_key=os.getenv("DMX_API_KEY"))  # 使用指定的模型和 API 密钥初始化聊天模型
+
+
+# template = "Write a {tone} email to {company} expressing interest in the {position} position, mentioning {skill} as a key strength. Keep it to 4 lines max"
+
+# prompt_template = ChatPromptTemplate.from_template(template)
+
+# prompt =  prompt_template.invoke({
+#     "tone": "energetic", 
+#     "company": "samsung", 
+#     "position": "AI Engineer", 
+#     "skill": "AI"
+# })
+
+# Example 2: Prompt with System and Human Messages (Using Tuples)
+messages = [
+    ("system", "You are a comedian who tells jokes about {topic}."),
+    ("human", "Tell me {joke_count} jokes."),
+]
+
+prompt_template = ChatPromptTemplate.from_messages(messages)
+prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
+result = llm.invoke(prompt)
+print(result)
